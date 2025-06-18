977e2351382c56c01b48c51e188816381d6cae22
Jhonilau <jhonilau21@gmail.com>
2025-06-18 10:48:18 +0700

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Metode tidak diizinkan' });
+  }
+
+  const { username, password } = req.body;
+
+  const users = [
+    { username: "jhonilau", password: "tester123" },
+    { username: "charlessusanto", password: "aleng123" },
+    { username: "adminkey", password: "admin123" }
+  ];
+
+  const found = users.find(u => u.username === username && u.password === password);
+  if (found) {
+    return res.status(200).json({ message: "Login berhasil" });
+  } else {
+    return res.status(401).json({ message: "Username atau password salah." });
+  }
+}
