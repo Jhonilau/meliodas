354e271ec74ca82d0083cb6b72e46dcd07d1e089
Jhonilau <jhonilau21@gmail.com>
2025-06-20 16:02:45 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,20 +1,6 @@
+import users from '../lib/users.js';
 import { setSession, getSession } from '../lib/session.js';
 import { v4 as uuidv4 } from 'uuid';
-
-const users = [
-  { username: "jhonilau", password: "tester123" },
-  { username: "singgoy", password: "polo123" },
-  { username: "brianadam", password: "cobates123" },
-  { username: "aleksusanto", password: "sadboy123" },
-  { username: "selanabila", password: "fendi188" },
-  { username: "harisetiadi", password: "bola123" },
-  { username: "selvieliza", password: "kerangwin" },
-  { username: "sitsun", password: "banteng69" },
-  { username: "charlessusanto", password: "aleng123" },
-  { username: "wisnuwd", password: "pucukubi123" },
-  { username: "wisnudp", password: "pucukkangkung123" },
-  { username: "wisnulc", password: "bodat123" }
-];
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
