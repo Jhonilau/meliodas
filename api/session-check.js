6c6d77166b43e62c84db2848254ede4b9211f2b4
Jhonilau <jhonilau21@gmail.com>
2025-06-20 15:15:38 +0700

    Update session-check.js

--- api/session-check.js
+++ api/session-check.js
@@ -1,15 +1,14 @@
-import redis from '../lib/redis.js';
+import { getRedis } from '../lib/redis.js';
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({ message: 'Method not allowed' });
+  if (req.method !== 'POST') return res.status(405).end();
 
-  const { username, token } = req.query;
-  if (!username || !token) return res.status(400).json({ message: 'Bad request' });
+  const { username, token } = req.body;
+  const savedToken = await getRedis(`session:${username}`);
 
-  const activeToken = await redis.get(`session:${username}`);
-  if (activeToken !== token) {
-    return res.status(403).json({ message: 'Token tidak valid' });
+  if (!savedToken || savedToken !== token) {
+    return res.status(403).json({ message: 'Sesi tidak valid.' });
   }
 
-  return res.status(200).json({ message: 'Token valid' });
+  return res.status(200).json({ message: 'Sesi valid.' });
 }
