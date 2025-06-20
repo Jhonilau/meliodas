e68db6423073930493808715db1af64e801bd833
Jhonilau <jhonilau21@gmail.com>
2025-06-20 13:25:44 +0700

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,11 +1,7 @@
-import { delRedis } from '../lib/redis';
+import redis from '../lib/redis';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Metode tidak diizinkan' });
-  }
-
   const { username } = req.body;
-  await delRedis(`active:${username}`);
+  await redis.del(`active:${username}`);
   return res.status(200).json({ message: 'Logout berhasil' });
 }
