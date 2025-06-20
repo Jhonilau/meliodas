559c00c2434bbed79c4069334ebe31bfee4179c2
Jhonilau <jhonilau21@gmail.com>
2025-06-20 14:46:16 +0700

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,14 +1,10 @@
-import { delRedis } from '../lib/redis.js';
+import redis from '../lib/redis';
 
 export default async function handler(req, res) {
   const { username } = req.body;
 
-  if (!username) return res.status(400).json({ message: 'Username dibutuhkan' });
+  if (!username) return res.status(400).json({ message: 'Username diperlukan' });
 
-  try {
-    await delRedis(`active:${username}`);
-    return res.status(200).json({ message: 'Logout berhasil' });
-  } catch (err) {
-    return res.status(500).json({ message: 'Gagal logout' });
-  }
+  await redis.del(`session:${username}`);
+  return res.status(200).json({ message: 'Logout berhasil' });
 }
