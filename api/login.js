11f1f0afd2bcbf0c062ef6d1e7118f7628dd9480
Jhonilau <jhonilau21@gmail.com>
2025-06-20 15:22:35 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,19 +1,29 @@
 import { v4 as uuidv4 } from 'uuid';
-import redis from '../lib/redis.js';
-import fs from 'fs';
-import path from 'path';
+import { setRedis, getRedis } from '../lib/redis.js';
+
+const users = [
+  { username: 'jhonilau', password: 'tester123' },
+  { username: 'singgoy', password: 'polo123' },
+  { username: 'brianadam', password: 'cobates123' },
+  { username: 'aleksusanto', password: 'sadboy123' },
+  { username: 'selanabila', password: 'fendi188' },
+  { username: 'harisetiadi', password: 'bola123' },
+  { username: 'selvieliza', password: 'kerangwin' },
+  { username: 'sitsun', password: 'banteng69' },
+  { username: 'charlessusanto', password: 'aleng123' },
+  { username: 'wisnuwd', password: 'pucukubi123' },
+  { username: 'wisnudp', password: 'pucukkangkung123' },
+  { username: 'wisnulc', password: 'bodat123' }
+];
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ message: 'Metode tidak diizinkan' });
-
-  const filePath = path.join(process.cwd(), 'api', 'users.json');
-  const users = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+  if (req.method !== 'POST') return res.status(405).end();
 
   const { username, password } = req.body;
   const user = users.find(u => u.username === username && u.password === password);
   if (!user) return res.status(401).json({ message: 'Username atau password salah.' });
 
   const token = uuidv4();
-  await redis.set(`session:${username}`, token);
+  await setRedis(`session:${username}`, token);
   return res.status(200).json({ message: 'Login berhasil', token });
 }
