569e191ea204491d7c90d4084c7f164fdad7c97e
Jhonilau <jhonilau21@gmail.com>
2025-06-20 12:40:07 +0700

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,26 +1,16 @@
-import fs from 'fs';
-import path from 'path';
+import redis from '../../lib/redis';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Metode tidak diizinkan' });
   }
 
   const { username } = req.body;
-  const filePath = path.join(process.cwd(), 'api', 'users.json');
 
   try {
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const users = JSON.parse(fileContent);
-    const userIndex = users.findIndex(u => u.username === username);
-
-    if (userIndex !== -1) {
-      users[userIndex].active = false;
-      fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
-    }
-
-    res.status(200).json({ message: "Logout berhasil" });
+    await redis.del(`user:${username}`);
+    return res.status(200).json({ message: 'Logout berhasil' });
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    return res.status(500).json({ message: 'Gagal logout.' });
   }
 }
